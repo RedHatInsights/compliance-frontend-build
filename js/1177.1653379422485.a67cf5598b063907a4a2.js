b7acfcd2255fa32892b4c97e82103b585ebc8087
Travis CI User <travis@example.org>
2022-05-24 08:08:17 +0000

    fix(ThresholdDescription): RHICOMPL-2836 description of threshold

---
+++ js/1177.1653379422485.a67cf5598b063907a4a2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[1177],{11177:()=>{}}]);
--- js/8223.1653309791100.8748bd33a523281b616b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[8223],{48223:()=>{}}]);
