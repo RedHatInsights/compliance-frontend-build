78bf20734534e88bff6830c4c0cd76d2daaac2bb
Travis CI User <travis@example.org>
2022-08-09 10:11:21 +0000

    chore: bump @redhat-cloud-services/frontend-components-utilities (#1724)
    Bumps [@redhat-cloud-services/frontend-components-utilities](https://github.com/RedHatInsights/frontend-components) from 3.2.23 to 3.2.24.
    - [Release notes](https://github.com/RedHatInsights/frontend-components/releases)
    - [Commits](https://github.com/RedHatInsights/frontend-components/commits)
    
    ---
    updated-dependencies:
    - dependency-name: "@redhat-cloud-services/frontend-components-utilities"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- js/5980.1660037997278.d9f840fb38abcfa5141e.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[5980,5467,9477,3929],{3674:(n,e,c)=>{var o=c(14636),p=c(280),r=c(98612);n.exports=function(n){return r(n)?o(n):p(n)}},45467:()=>{}}]);
-//# sourceMappingURL=../sourcemaps/5980.c40e406d636e8f0355731442db7ed882.js.map
